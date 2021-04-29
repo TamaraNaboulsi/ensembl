f96205a09fcb1029013688f60ceb493b160161b6
Tamara El Naboulsi <48948777+TamaraNaboulsi@users.noreply.github.com>
2021-04-29 09:48:40 +0100

    Update Exon.pm

--- modules/Bio/EnsEMBL/Exon.pm
+++ modules/Bio/EnsEMBL/Exon.pm
@@ -1298,7 +1298,9 @@ sub is_coding {
 
   if (!$transcript) { throw("Transcript parameter is required for " . __PACKAGE__ . "->is_coding()."); }
 
-  if (!$transcript->can(translate) || !$transcript->translate) { return 0; }
+  if ($transcript->can(translate)) {
+  	if (!$transcript->translate) { return 0; }
+  }
 
   # coding region overlaps start of exon
   if ($transcript->coding_region_start <= $self->start && $self->start <= $transcript->coding_region_end) { return 1; }
